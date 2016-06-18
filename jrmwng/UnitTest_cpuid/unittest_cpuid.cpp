febcdf913d5cfa498e95c5d65e9990fd6b8f289f
jrmwng <jmw@netvigator.com>
2016-06-18 23:36:01 +0800

    change type from int to unsigned, add test cases, reduce depth of hierarchy

--- jrmwng/UnitTest_cpuid/unittest_cpuid.cpp
+++ jrmwng/UnitTest_cpuid/unittest_cpuid.cpp
@@ -15,7 +15,23 @@ namespace UnitTest_cpuid
 		{
 			using namespace jrmwng::cpuid;
 
-			cpuid_forest_t cpuidForest;
+			cpuid_forest_t cpuidForest0;
+			std::stringstream ss;
+			{
+				ss << cpuidForest0;
+				ss.seekp(0);
+			}
+			cpuid_forest_t cpuidForest1(ss);
+
+			cpuid_standard_tree_t cpuidStandardTree = cpuidForest1;
+			cpuid_extended_tree_t cpuidExtendedTree = cpuidForest1;
+			cpuid_hypervisor_tree_t cpuidHypervisorTree = cpuidForest1;
+
+			cpuid_t<0x00000000U> cpuid00000000 = cpuidStandardTree;
+			cpuid_t<0x00000001U> cpuid00000001 = cpuidStandardTree;
+			cpuid_t<0x80000000U> cpuid80000000 = cpuidExtendedTree;
+			cpuid_t<0x40000000U> cpuid40000000 = cpuidHypervisorTree;
+
 
 			// TODO: Your test code here
 		}
