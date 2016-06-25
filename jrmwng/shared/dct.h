9850c98411f4cad927f5e8e3a80b734f4864191b
jrmwng <jmw@netvigator.com>
2016-06-25 13:21:10 +0800

    rename from for_each(...) to dct_for_each(...)

--- jrmwng/shared/dct.h
+++ jrmwng/shared/dct.h
@@ -12,7 +12,7 @@ namespace jrmwng
 	namespace
 	{
 		template <typename Tinteger, Tinteger... tInteger, typename Tfunc>
-		void for_each(std::integer_sequence<Tinteger, tInteger...>, Tfunc && tFunc)
+		void dct_for_each(std::integer_sequence<Tinteger, tInteger...>, Tfunc && tFunc)
 		{
 			using type = int [];
 			(void) type {
@@ -57,9 +57,9 @@ namespace jrmwng
 
 		dct_matrix()
 		{
-			for_each(std::make_index_sequence<uN>(), [&](auto const tN)
+			dct_for_each(std::make_index_sequence<uN>(), [&](auto const tN)
 			{
-				for_each(std::make_index_sequence<uN>(), [&, tN](auto const tQ)
+				dct_for_each(std::make_index_sequence<uN>(), [&, tN](auto const tQ)
 				{
 					m_aat[tN][tQ] = dct_coefficient<T, uN>(tQ, tN);
 				});
@@ -77,11 +77,11 @@ namespace jrmwng
 	{
 		auto const & dctMatrix = dct_matrix<T, uN>::g_Instance;
 
-		for_each(std::make_index_sequence<uN>(), [&](auto const k)
+		dct_for_each(std::make_index_sequence<uN>(), [&](auto const k)
 		{
 			T tAcc(0);
 			{
-				for_each(std::make_index_sequence<uN>(), [&, k](auto const n)
+				dct_for_each(std::make_index_sequence<uN>(), [&, k](auto const n)
 				{
 					tAcc += atInput[n] * dctMatrix[n][k];
 				});
@@ -94,15 +94,15 @@ namespace jrmwng
 	{
 		auto const & dctMatrix = dct_matrix<double, (uN4 * 4)>::g_Instance;
 
-		for_each(std::make_index_sequence<uN4>(), [&](auto const k4)
+		dct_for_each(std::make_index_sequence<uN4>(), [&](auto const k4)
 		{
 			__m256d lr4Acc = _mm256_setzero_pd();
 			{
-				for_each(std::make_index_sequence<uN4>(), [&](auto const n4)
+				dct_for_each(std::make_index_sequence<uN4>(), [&](auto const n4)
 				{
 					__m256d const lr4InputABCD = alr4Input[n4];
 
-					for_each(std::make_index_sequence<4>(), [&](auto const n1)
+					dct_for_each(std::make_index_sequence<4>(), [&](auto const n1)
 					{
 						__m256d const lr4Input = _mm256_permute4x64_pd(lr4InputABCD, _MM_SHUFFLE(n1.value, n1.value, n1.value, n1.value));
 						__m256d const lr4Coeff = _mm256_load_pd(dctMatrix[n4 * 4 + n1] + k4 * 4);
@@ -120,15 +120,15 @@ namespace jrmwng
 	{
 		auto const & dctMatrix = dct_matrix<float, (uN8 * 8)>::g_Instance;
 
-		for_each(std::make_index_sequence<uN8>(), [&](auto const k8)
+		dct_for_each(std::make_index_sequence<uN8>(), [&](auto const k8)
 		{
 			__m256 r8Acc = _mm256_setzero_ps();
 			{
-				for_each(std::make_index_sequence<uN8>(), [&](auto const n8)
+				dct_for_each(std::make_index_sequence<uN8>(), [&](auto const n8)
 				{
 					__m256 const r8InputABCDEFGH = ar8Input[n8];
 
-					for_each(std::make_integer_sequence<unsigned, 8>(), [&](auto const n1)
+					dct_for_each(std::make_integer_sequence<unsigned, 8>(), [&](auto const n1)
 					{
 						__m256 const r8Input = _mm256_permutevar8x32_ps(r8InputABCDEFGH, _mm256_set1_epi32(n1));
 						__m256 const r8Coeff = _mm256_load_ps(dctMatrix[n8 * 8 + n1] + k8 * 8);
@@ -146,15 +146,15 @@ namespace jrmwng
 	{
 		auto const & dctMatrix = dct_matrix<double, (uN2 * 2)>::g_Instance;
 
-		for_each(std::make_index_sequence<uN2>(), [&](auto const k2)
+		dct_for_each(std::make_index_sequence<uN2>(), [&](auto const k2)
 		{
 			__m128d lr2Acc = _mm_setzero_pd();
 			{
-				for_each(std::make_index_sequence<uN2>(), [&](auto const n2)
+				dct_for_each(std::make_index_sequence<uN2>(), [&](auto const n2)
 				{
 					__m128d const lr2InputAB = alr2Input[n2];
 
-					for_each(std::make_index_sequence<2>(), [&](auto const n1)
+					dct_for_each(std::make_index_sequence<2>(), [&](auto const n1)
 					{
 						__m128d const lr2Input = _mm_shuffle_pd(lr2InputAB, lr2InputAB, _MM_SHUFFLE2(n1.value, n1.value));
 						__m128d const lr2Coeff = _mm_load_pd(dctMatrix[n2 * 2 + n1] + k2 * 2);
@@ -172,15 +172,15 @@ namespace jrmwng
 	{
 		auto const & dctMatrix = dct_matrix<float, (uN4 * 4)>::g_Instance;
 
-		for_each(std::make_index_sequence<uN4>(), [&](auto const k4)
+		dct_for_each(std::make_index_sequence<uN4>(), [&](auto const k4)
 		{
 			__m128 r4Acc = _mm_setzero_ps();
 			{
-				for_each(std::make_index_sequence<uN4>(), [&](auto const n4)
+				dct_for_each(std::make_index_sequence<uN4>(), [&](auto const n4)
 				{
 					__m128 const r4InputABCD = ar4Input[n4];
 
-					for_each(std::make_index_sequence<4>(), [&](auto const n1)
+					dct_for_each(std::make_index_sequence<4>(), [&](auto const n1)
 					{
 						__m128 const r4Input = _mm_shuffle_ps(r4InputABCD, r4InputABCD, _MM_SHUFFLE(n1.value, n1.value, n1.value, n1.value));
 						__m128 const r4Coeff = _mm_load_ps(dctMatrix[n4 * 4 + n1] + k4 * 4);
