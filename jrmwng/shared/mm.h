df47ba1d55f426accaee868a54175ee3d1487c9e
jrmwng <jmw@netvigator.com>
2016-07-02 11:31:11 +0800

    remove redundent constructor overloads

--- jrmwng/shared/mm.h
+++ jrmwng/shared/mm.h
@@ -272,16 +272,8 @@ namespace jrmwng
 			//
 
 			template <typename T1, typename T2>
-			Mvar(Mop<std::plus, T1, Mop<std::negate, T2>> const & tOp)
-				: m_mmValue(Mtraits<Tmm, T>::minus(Mvar(get<0>(tOp)), Mvar(get<0>(get<1>(tOp)))))
-			{}
-			template <typename T1, typename T2>
 			Mvar(Mop<std::plus, T1, T2> const & tOp)
 				: m_mmValue(Mtraits<Tmm, T>::plus(Mvar(get<0>(tOp)), Mvar(get<1>(tOp))))
-			{}
-			template <typename T1, typename T2>
-			Mvar(Mop<std::minus, T1, Mop<std::negate, T2>> const & tOp)
-				: m_mmValue(Mtraits<Tmm, T>::plus(Mvar(get<0>(tOp)), Mvar(get<0>(get<1>(tOp)))))
 			{}
 			template <typename T1, typename T2>
 			Mvar(Mop<std::minus, T1, T2> const & tOp)
