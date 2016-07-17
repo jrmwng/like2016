a5f0cff42089821f1314d08a8271afdd36f063d9
jrmwng <jmw@netvigator.com>
2016-07-17 11:27:33 +0800

    Improve readability (refactor variables and add comments)

--- jrmwng/sudoku/sudoku.h
+++ jrmwng/sudoku/sudoku.h
@@ -109,26 +109,29 @@ namespace jrmwng
 
 					if (TT::SUDOKU_CANDIDATE_COUNT == 9)
 					{
-						std::tuple<__m128i, __m128i, __m128i> axmmCombinedCandidateSet;
+						// union set of candidate sets of selected cell(s)
+						std::tuple<__m128i, __m128i, __m128i> axmmUnionCandidateSet;
+						// belonging group set of individual cell
 						std::tuple<__m128i, __m128i, __m128i> axmmCellGroupSet;
+						// candidate set of individual cell
 						std::tuple<__m128i, __m128i, __m128i> axmmCellCandidateSet;
 						{
 							sudoku_for_each(std::make_index_sequence<3>(), [&](auto const i)
 							{
 								// alCell[0].lCandidateSet 0 alCell[0].lCandidateSet 0 alCell[0].lCandidateSet 0 alCell[0].lCandidateSet 0
 								// [123456789] [] [123456789] [] [123456789] [] [123456789] []
-								__m128i const xmmCandidateSet0 = _mm_unpacklo_epi16(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 0]].lCandidateSet), 0));
+								__m128i const xmmCellCandidateSet147 = _mm_unpacklo_epi16(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 0]].lCandidateSet), _MM_SHUFFLE(0, 0, 0, 0)));
 								// alCell[1].lCandidateSet alCell[1].lCandidateSet 0 0 alCell[1].lCandidateSet alCell[1].lCandidateSet 0 0
 								// [123456789] [123456789] [] [] [123456789] [123456789] [] []
-								__m128i const xmmCandidateSet1 = _mm_unpacklo_epi32(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 1]].lCandidateSet), 0));
+								__m128i const xmmCellCandidateSet258 = _mm_unpacklo_epi32(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 1]].lCandidateSet), _MM_SHUFFLE(0, 0, 0, 0)));
 								// alCell[2].lCandidateSet alCell[2].lCandidateSet alCell[2].lCandidateSet alCell[2].lCandidateSet 0 0 0 0
 								// [123456789] [123456789] [123456789] [123456789] [] [] [] []
-								__m128i const xmmCandidateSet2 = _mm_unpacklo_epi64(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 2]].lCandidateSet), 0));
+								__m128i const xmmCellCandidateSet369 = _mm_unpacklo_epi64(_mm_setzero_si128(), _mm_shufflelo_epi16(_mm_cvtsi32_si128(astCell[stDirtyGroup.alCellIndex[i * 3 + 2]].lCandidateSet), _MM_SHUFFLE(0, 0, 0, 0)));
 
 								// 01FF 0000 01FF 0000 01FF 0000 01FF 0000
 								// 01FF 01FF 0000 0000 01FF 01FF 0000 0000
 								// 01FF 01FF 01FF 01FF 0000 0000 0000 0000
-								__m128i const xmmCombinedCandidateSet = _mm_or_si128(_mm_or_si128(xmmCandidateSet0, xmmCandidateSet1), xmmCandidateSet2);
+								__m128i const xmmUnionCandidateSet = _mm_or_si128(_mm_or_si128(xmmCellCandidateSet147, xmmCellCandidateSet258), xmmCellCandidateSet369);
 
 								// 0 alCell[2].lGroupSet alCell[1].lGroupSet alCell[0].lGroupSet
 								// 0 box[0]col[2]row[0] box[0]col[1]row[0] box[0]col[0]row[0]
@@ -136,64 +139,76 @@ namespace jrmwng
 								__m128i const xmmCellGroupSet = _mm_set_epi32(0, astCell[stDirtyGroup.alCellIndex[i * 3 + 2]].lGroupSet, astCell[stDirtyGroup.alCellIndex[i * 3 + 1]].lGroupSet, astCell[stDirtyGroup.alCellIndex[i * 3 + 0]].lGroupSet);
 
 
-								std::get<i.value>(axmmCombinedCandidateSet) = xmmCombinedCandidateSet;
+								std::get<i.value>(axmmUnionCandidateSet) = xmmUnionCandidateSet;
 								std::get<i.value>(axmmCellGroupSet) = xmmCellGroupSet;
 
 								// 0000 0000 0000 0000 0000 0000 0000 01FF
 								// 0000 0000 0000 0000 0000 01FF 0000 0000
 								// 0000 0000 0000 01FF 0000 0000 0000 0000
-								std::get<i.value>(axmmCellCandidateSet) = _mm_and_si128(_mm_shufflelo_epi16(xmmCombinedCandidateSet, _MM_SHUFFLE(0, 2, 0, 1)), _mm_set_epi32(0, 0xFFFF, ~0, ~0));
+								std::get<i.value>(axmmCellCandidateSet) = _mm_and_si128(_mm_shufflelo_epi16(xmmUnionCandidateSet, _MM_SHUFFLE(0, 2, 0, 1)), _mm_set_epi32(0, 0xFFFF, ~0, ~0));
 							});
 						}
 
 						__m128i xmmLocalDirtyGroupSet = _mm_setzero_si128();
-						for (long lCellSet789 = 0; lCellSet789 < (1 << TT::SUDOKU_CANDIDATE_COUNT); lCellSet789 += (1 << 6), std::get<2>(axmmCombinedCandidateSet) = _mm_alignr_epi8(std::get<2>(axmmCombinedCandidateSet), std::get<2>(axmmCombinedCandidateSet), 2))
+						for (long lCellSet789 = 0; lCellSet789 < (1 << TT::SUDOKU_CANDIDATE_COUNT); lCellSet789 += (1 << 6), std::get<2>(axmmUnionCandidateSet) = _mm_alignr_epi8(std::get<2>(axmmUnionCandidateSet), std::get<2>(axmmUnionCandidateSet), 2))
 						{
-							long const lCandidateSet789 = _mm_extract_epi16(std::get<2>(axmmCombinedCandidateSet), 0);
+							long const lCandidateSet789 = _mm_extract_epi16(std::get<2>(axmmUnionCandidateSet), 0);
 							unsigned const uMinCntCell789 = __popcnt(lCellSet789);
 							unsigned const uMaxCntCell789 = __popcnt(lCellSet789) + 6;
 							unsigned const uMinCntCandidate789 = __popcnt(lCandidateSet789);
-							unsigned const uMaxCntCandidate789 = __popcnt(lCandidateSet789 | _mm_extract_epi16(_mm_or_si128(std::get<1>(axmmCombinedCandidateSet), std::get<0>(axmmCombinedCandidateSet)), 7));
+							unsigned const uMaxCntCandidate789 = __popcnt(lCandidateSet789 | _mm_extract_epi16(_mm_or_si128(std::get<1>(axmmUnionCandidateSet), std::get<0>(axmmUnionCandidateSet)), 7));
 
 							if (uMinCntCandidate789 <= uMaxCntCell789 &&
 								uMinCntCell789 <= uMaxCntCandidate789)
 							{
-								for (long lCellSet456789 = lCellSet789; lCellSet456789 < lCellSet789 + (1 << 6); lCellSet456789 += (1 << 3), std::get<1>(axmmCombinedCandidateSet) = _mm_alignr_epi8(std::get<1>(axmmCombinedCandidateSet), std::get<1>(axmmCombinedCandidateSet), 2))
+								for (long lCellSet456789 = lCellSet789; lCellSet456789 < lCellSet789 + (1 << 6); lCellSet456789 += (1 << 3), std::get<1>(axmmUnionCandidateSet) = _mm_alignr_epi8(std::get<1>(axmmUnionCandidateSet), std::get<1>(axmmUnionCandidateSet), 2))
 								{
-									long const lCandidateSet456789 = lCandidateSet789 | _mm_extract_epi16(std::get<1>(axmmCombinedCandidateSet), 0);
+									long const lCandidateSet456789 = lCandidateSet789 | _mm_extract_epi16(std::get<1>(axmmUnionCandidateSet), 0);
 									unsigned const uMinCntCell456789 = __popcnt(lCellSet456789);
 									unsigned const uMaxCntCell456789 = __popcnt(lCellSet456789) + 3;
 									unsigned const uMinCntCandidate456789 = __popcnt(lCandidateSet456789);
-									unsigned const uMaxCntCandidate456789 = __popcnt(lCandidateSet456789 | _mm_extract_epi16(std::get<0>(axmmCombinedCandidateSet), 7));
+									unsigned const uMaxCntCandidate456789 = __popcnt(lCandidateSet456789 | _mm_extract_epi16(std::get<0>(axmmUnionCandidateSet), 7));
 
 									if (uMinCntCandidate456789 <= uMaxCntCell456789 &&
 										uMinCntCell456789 <= uMaxCntCandidate456789)
 									{
-										for (long lCellSet123456789 = lCellSet456789; lCellSet123456789 < lCellSet456789 + (1 << 3); lCellSet123456789++, std::get<0>(axmmCombinedCandidateSet) = _mm_alignr_epi8(std::get<0>(axmmCombinedCandidateSet), std::get<0>(axmmCombinedCandidateSet), 2))
+										for (long lCellSet123456789 = lCellSet456789; lCellSet123456789 < lCellSet456789 + (1 << 3); lCellSet123456789++, std::get<0>(axmmUnionCandidateSet) = _mm_alignr_epi8(std::get<0>(axmmUnionCandidateSet), std::get<0>(axmmUnionCandidateSet), 2))
 										{
-											long const lCandidateSet123456789 = lCandidateSet456789 | _mm_extract_epi16(std::get<0>(axmmCombinedCandidateSet), 0);
+											long const lCandidateSet123456789 = lCandidateSet456789 | _mm_extract_epi16(std::get<0>(axmmUnionCandidateSet), 0);
 											unsigned const uPopCntCell123456789 = __popcnt(lCellSet123456789);
 											unsigned const uPopCntCandidate123456789 = __popcnt(lCandidateSet123456789);
 
-											__m128i const xmmCellSet = _mm_set1_epi32(lCellSet123456789);
-											__m128i const xmmCandidateSet = _mm_set1_epi32(lCandidateSet123456789);
+											__m128i const xmmActiveCellSet = _mm_set1_epi32(lCellSet123456789);
+											// 000001FF 000001FF 000001FF 000001FF
+											__m128i const xmmUnionCandidateSet = _mm_set1_epi32(lCandidateSet123456789);
 
 											if (uPopCntCandidate123456789 == uPopCntCell123456789)
 											{
 												sudoku_for_each(std::make_index_sequence<3>(), [&](auto const i)
 												{
-													// 00000000 00000004 00000002 00000001
-													// 00000000 00000020 00000010 00000008
-													// 00000000 00000100 00000080 00000040
-													__m128i const xmmCellBit = _mm_and_si128(xmmCellSet, _mm_set_epi32(0, (0x004 << (i * 3)), (0x002 << (i * 3)), (0x001 << (i * 3))));
+													// [i=0] 00000000 00000004 00000002 00000001
+													// [i=1] 00000000 00000020 00000010 00000008
+													// [i=2] 00000000 00000100 00000080 00000040
+													__m128i const xmmActiveCellBit = _mm_and_si128(xmmActiveCellSet, _mm_set_epi32(0, (0x004 << (i * 3)), (0x002 << (i * 3)), (0x001 << (i * 3))));
 
-													__m128i const xmmOtherCellMask = _mm_cmpeq_epi32(xmmCellBit, _mm_setzero_si128());
+													// FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF
+													// FFFFFFFF FFFFFFFF FFFFFFFF 00000000
+													// FFFFFFFF FFFFFFFF 00000000 FFFFFFFF
+													// FFFFFFFF FFFFFFFF 00000000 00000000
+													// FFFFFFFF 00000000 FFFFFFFF FFFFFFFF
+													// FFFFFFFF 00000000 FFFFFFFF 00000000
+													// FFFFFFFF 00000000 00000000 FFFFFFFF
+													// FFFFFFFF 00000000 00000000 00000000
+													__m128i const xmmInactiveCellMask = _mm_cmpeq_epi32(xmmActiveCellBit, _mm_setzero_si128());
 
-													__m128i const xmmClearCandidateInOtherCell = _mm_and_si128(xmmOtherCellMask, xmmCandidateSet);
+													// 000001FF 000001FF 000001FF 00000000
+													// 000001FF 000001FF 00000000 000001FF
+													// 000001FF 00000000 000001FF 000001FF
+													__m128i const xmmClearCandidateOfInactiveCell = _mm_and_si128(xmmInactiveCellMask, xmmUnionCandidateSet);
 
-													__m128i const xmmNewCandidateSet = _mm_andnot_si128(xmmClearCandidateInOtherCell, std::get<i.value>(axmmCellCandidateSet));
+													__m128i const xmmNewCandidateSet = _mm_andnot_si128(xmmClearCandidateOfInactiveCell, std::get<i.value>(axmmCellCandidateSet));
 
-													__m128i const xmmChangeGroupMask = _mm_cmplt_epi32(xmmNewCandidateSet, std::get<i.value>(axmmCellCandidateSet)); // because bit-31 is not used
+													__m128i const xmmChangeGroupMask = _mm_cmplt_epi32(xmmNewCandidateSet, std::get<i.value>(axmmCellCandidateSet)); // because bit-31 is not used and we are clearing bit(s)
 
 													std::get<i.value>(axmmCellCandidateSet) = xmmNewCandidateSet;
 
